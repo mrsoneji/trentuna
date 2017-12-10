11f33c4e1295272bc8a85ba9d084170a3236a7f9
angelraver <angeldavidflorentin@gmail.com>
2017-12-10 16:39:58 -0300

    puntajes y desbloqueos

--- menu.lua
+++ menu.lua
@@ -1,12 +1,12 @@
-local composer = require( "composer" )
+local composer = require( 'composer' )
 local scene = composer.newScene()
-local utility = require( "utility" )
-local uiSettings = require( "uiSettings" )
-local animacion = require( "animacion" )
+local uiSettings = require( 'uiSettings' )
+local utility = require('utility')
+local animacion = require( 'animacion' )
 local sonidos = require('sonidosSettings')
-local params
-audio.setVolume(0.2)
+audio.setVolume(0.5)
 
+local myData = utility.loadSettings()
 local function PlayEvent( event )
     if ( "ended" == event.phase ) then
         audio.play(sonidos.effects.ui.menuSelect)
@@ -32,56 +32,59 @@ local function BookEvent( event )
     end
 end
 
+local title = animacion.crear(uiSettings.title)
+title.x = display.contentCenterX
+title.y = 70
+audio.play(sonidos.themes.menu, {loops = -1 , channel = sonidos.channels.background} )
+
+local play = display.newImage(uiSettings.play)
+play.x = display.contentCenterX
+play.y = display.contentCenterY + 20
+play:addEventListener('touch', PlayEvent)
+
+local options = display.newImage(uiSettings.options)
+options.x = display.contentCenterX
+options.y = display.contentCenterY + 70
+options:addEventListener('touch', OptionsEvent)
+
+local book = display.newImage(uiSettings.stickerbook)
+book.x = display.contentCenterX + 170
+book.y = display.contentCenterY + 70
+book.alpha = 0
+book:addEventListener('touch', BookEvent)
+
+local bookBlocked = display.newImage(uiSettings.stickerbookblocked)
+bookBlocked.x = display.contentCenterX + 170
+bookBlocked.y = display.contentCenterY + 70
+bookBlocked.alpha = 0
+
+local fondo = display.newImage( 'images/scenarios/1.jpg' )
+fondoPivotX = fondo.contentWidth / 2
+fondoPivotY = fondo.contentHeight / 2
+fondo.y = fondoPivotY
+fondo.x = fondoPivotX
+transition.to(fondo, {time = 100000, xScale = 1, yScale = 1, y = -320, x = 455 })
+
 function scene:create( event )
     local sceneGroup = self.view
-
-    audio.play(sonidos.themes.menu, {loops = -1 , channel = sonidos.channels.background} )
-
-
-    params = event.params
-
-    fondo = display.newImage( "images/scenarios/1.jpg" )
-    fondoPivotX = fondo.contentWidth / 2
-    fondoPivotY = fondo.contentHeight / 2
-    fondo.y = fondoPivotY
-    fondo.x = fondoPivotX
-    transition.to(fondo, {time = 100000, xScale = 1, yScale = 1, y = -320, x = 455 })
     sceneGroup:insert(fondo)
-
-    local title = animacion.crear(uiSettings.title)
-    title.x = display.contentCenterX
-    title.y = 70
     sceneGroup:insert( title )
-
-    local play = display.newImage(uiSettings.play)
-    play.x = display.contentCenterX
-    play.y = display.contentCenterY + 20
-    play:addEventListener("touch", PlayEvent)
     sceneGroup:insert( play )
-
-    local options = display.newImage(uiSettings.options)
-    options.x = display.contentCenterX
-    options.y = display.contentCenterY + 70
-    options:addEventListener("touch", OptionsEvent)
     sceneGroup:insert( options )
-
-    local book = display.newImage(uiSettings.stickerbookblocked)
-    book.x = display.contentCenterX + 170
-    book.y = display.contentCenterY + 70
-    book:addEventListener("touch", BookEvent)
     sceneGroup:insert( book )
+    sceneGroup:insert( bookBlocked )
 end
 
 function scene:show( event )
     local sceneGroup = self.view
-    if event.phase == "did" then
-        composer.removeScene( "game" ) 
+    if event.phase == 'did' then
+        composer.removeScene( 'game' ) 
     end
 end
 
 function scene:hide( event )
     local sceneGroup = self.view
-    if event.phase == "will" then
+    if event.phase == 'will' then
     end
 end
 
@@ -89,8 +92,8 @@ function scene:destroy( event )
     local sceneGroup = self.view
 end
 
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
-scene:addEventListener( "destroy", scene )
+scene:addEventListener( 'create', scene )
+scene:addEventListener( 'show', scene )
+scene:addEventListener( 'hide', scene )
+scene:addEventListener( 'destroy', scene )
 return scene
